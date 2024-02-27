539816eb44196bea0f062fd48c52b1f60a4cf85a
Tinu C Mathew <tinucherian229@gmail.com>
2024-02-27 23:31:12 +0530

    small changes

--- script.js
+++ script.js
@@ -7,13 +7,12 @@ document.addEventListener("DOMContentLoaded", function() {
         const messageDiv = document.createElement("div");
         messageDiv.textContent = "USER : " + message;
         messageDiv.className = 'user-message';
-        // Append the new message div to the messages container
+    
         messagesDiv.appendChild(messageDiv);
     
-        // No need for setTimeout to change opacity if CSS animation is correctly set up
-        // The CSS animation should automatically play when the element is added to the document
     
-        messagesDiv.scrollTop = messagesDiv.scrollHeight; // Ensure the new message is visible
+    
+        messagesDiv.scrollTop = messagesDiv.scrollHeight;
     }
     
     
@@ -22,13 +21,13 @@ document.addEventListener("DOMContentLoaded", function() {
         const message = userInput.value.trim();
         if (message) {
             displayUserMessage(message);
-            userInput.value = ''; // Clear the input field after sending
+            userInput.value = '';
         }
     });
 
     userInput.addEventListener("keypress", function(e) {
         if (e.key === "Enter") {
-            e.preventDefault(); // Avoid form submission behavior
+            e.preventDefault(); 
             sendButton.click();
         }
     });
